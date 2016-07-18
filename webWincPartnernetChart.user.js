c182a9f1ad465e6b3f49a1aa35f95b260259a229
Sawascwoolf <admin@forumkl.de>
2016-07-18 00:11:42 -0700

    Add files via upload

---
+++ webWincPartnernetChart.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        webWincPartnernetChart
+// @namespace   webWinc
+// @include     https://partnernet.amazon.de/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+alert("running");
